ca079813fe403331ff8c5d00616dbabdac2ae69b
demaxiya123 <11441514@qq.com>
2017-05-22 15:03:22 -0700

    Initial Commit

---
+++ Lyx_PlaneWar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lyx_PlaneWar
+//
+//  Created by liaoyongxing on 2017/5/22.
+//  Copyright © 2017年 Lyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lyx_PlaneWarTests/Lyx_PlaneWarTests.m
@@ -0,0 +1,39 @@
+//
+//  Lyx_PlaneWarTests.m
+//  Lyx_PlaneWarTests
+//
+//  Created by liaoyongxing on 2017/5/22.
+//  Copyright © 2017年 Lyx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lyx_PlaneWarTests : XCTestCase
+
+@end
+
+@implementation Lyx_PlaneWarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
